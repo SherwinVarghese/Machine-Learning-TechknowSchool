eb31423429b94fa5e299a905c007cdfa1100f6fd
Harshit Joshi <harshit.joshi@sap.com>
2019-06-09 17:39:29 +0530

    Add files via upload

---
+++ modules/7. Demo/APPLY_MODEL - Guide.sql
@@ -0,0 +1,38 @@
+-- --------------------------------------------------------------------------
+-- Create table type for the dataset
+-- --------------------------------------------------------------------------
+-- Input table type: dataset
+
+
+-- Ouput table type: dataset
+
+
+
+
+
+-- --------------------------------------------------------------------------
+-- Create AFL wrappers for the APL function
+-- --------------------------------------------------------------------------
+-- the AFL wrapper generator needs the signature of the expected stored proc
+
+
+
+
+
+-- --------------------------------------------------------------------------
+-- Create the input/output tables used as arguments for the APL function
+-- --------------------------------------------------------------------------
+
+
+
+
+
+
+
+-- --------------------------------------------------------------------------
+-- Execute the APL function using its AFL wrapper and the actual input/output tables
+-- --------------------------------------------------------------------------
+
+
+
+
