41cffafbd31aee9eeb6dbd10ee2912edf5732d56
Harshit Joshi <harshit.joshi@sap.com>
2019-06-10 10:24:32 +0530

    Add files via upload

---
+++ modules/7. Demo/APL/CREATE_MODEL_AND_TRAIN - Guide.sql
@@ -0,0 +1,35 @@
+-- Input table type: dataset
+-- Define the table type for the input dataset (training dataset)
+
+
+
+
+
+
+-- --------------------------------------------------------------------------
+-- Create AFL wrappers for the APL function
+-- --------------------------------------------------------------------------
+-- the AFL wrapper generator needs the signature of the expected stored proc
+
+
+
+
+
+
+-- --------------------------------------------------------------------------
+-- Create the input/output tables used as arguments for the APL function
+-- --------------------------------------------------------------------------
+
+
+
+
+
+
+-- --------------------------------------------------------------------------
+-- Execute the APL function using its AFL wrapper and the actual input/output tables
+-- --------------------------------------------------------------------------
+
+
+
+
+
